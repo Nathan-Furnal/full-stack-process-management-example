38fc54d932d6b8774c6f3d924f252fed806e3e62
Nathan Furnal <nathanfurnal.dev@gmail.com>
2024-05-10 12:16:07 +0200

    fix engine for tests

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,14 +1,28 @@
 import pytest
-from sqlalchemy import create_engine
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine, StaticPool
 
-from backend.db import Base
+from backend.db import Base, get_engine
+from backend.main import app
 
 
+# https://fastapi.tiangolo.com/advanced/testing-database/
 @pytest.fixture(scope="session")
 def engine():
-    engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+    engine = create_engine(
+        "sqlite+pysqlite:///:memory:",
+        echo=True,
+        poolclass=StaticPool,
+        connect_args={"check_same_thread": False},
+    )
     try:
-        Base.metadata.create_all(engine)
+        Base.metadata.create_all(bind=engine)
         yield engine
     finally:
-        Base.metadata.drop_all(engine)
+        Base.metadata.drop_all(bind=engine)
+
+
+@pytest.fixture
+def webclient(engine):
+    app.dependency_overrides[get_engine] = lambda: engine
+    return TestClient(app)
