8ba4c8945ab66b2747a553c2daf11f98bad1485f
Nathan Furnal <nathanfurnal.dev@gmail.com>
2024-05-10 12:44:25 +0200

    use sessions instead of engine directly

--- backend/backend/routers/processes/router.py
+++ backend/backend/routers/processes/router.py
@@ -1,27 +1,24 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
-from sqlalchemy import Engine
 from sqlalchemy.orm import Session
 
-from backend.db import get_engine
+from backend.db import get_session
 from backend.models import Process
 from backend.routers.processes import GetProcess
 
-EngineDep = Annotated[Engine, Depends(get_engine)]
+SessionDep = Annotated[Session, Depends(get_session)]
 
 router = APIRouter(prefix="/processes")
 
 
 @router.get("/", response_model=list[GetProcess])
-async def get_all_processes(engine: EngineDep):
-    with Session(engine) as session:
-        processes = session.query(Process).all()
-        return [GetProcess.model_validate(p) for p in processes]
+async def get_all_processes(session: SessionDep):
+    processes = session.query(Process).all()
+    return [GetProcess.model_validate(p) for p in processes]
 
 
 @router.get("/{pid}", response_model=GetProcess)
-async def get_process(pid: int, engine: EngineDep):
-    with Session(engine) as session:
-        process = session.get(Process, pid)
-        return GetProcess.model_validate(process)
+async def get_process(pid: int, session: SessionDep):
+    process = session.get(Process, pid)
+    return GetProcess.model_validate(process)
