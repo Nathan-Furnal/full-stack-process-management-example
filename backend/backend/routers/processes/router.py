26cc60667d18c1dc9626adc08f7084f7bb00aced
Nathan Furnal <nathanfurnal.dev@gmail.com>
2024-05-16 16:44:10 +0200

    add route to create process

--- backend/backend/routers/processes/router.py
+++ backend/backend/routers/processes/router.py
@@ -4,12 +4,38 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from backend.db import get_session
-from backend.models import Process
-from backend.routers.processes import GetProcess
+from backend.models import Attachment, Event, Link, Process
+from backend.routers.processes import GetProcess, PostProcess
 
 SessionDep = Annotated[Session, Depends(get_session)]
 
 router = APIRouter(prefix="/processes")
+
+
+def create_process_from_payload(session: Session, payload: PostProcess):
+    events = []
+    for event in payload.events:
+        links = [Link(url=link.url) for link in event.links]
+        attachments = [Attachment(filename=a.filename) for a in event.attachments]
+        db_event = Event(
+            type=event.type,
+            explanation=event.explanation,
+            links=links,
+            attachments=attachments,
+        )
+        events.append(db_event)
+    session.add_all(events)
+
+    db_process = Process(
+        business_date=payload.business_date,
+        working_date=payload.working_date,
+        service=payload.service,
+        performance=payload.performance,
+        events=events,
+    )
+    session.add(db_process)
+    session.commit()
+    return GetProcess.model_validate(db_process)
 
 
 @router.get("/", response_model=list[GetProcess])
@@ -22,3 +48,8 @@ async def get_all_processes(session: SessionDep):
 async def get_process(pid: int, session: SessionDep):
     process = session.get(Process, pid)
     return GetProcess.model_validate(process)
+
+
+@router.post("/", response_model=GetProcess)
+async def create_process(payload: PostProcess, session: SessionDep):
+    return create_process_from_payload(session=session, payload=payload)
