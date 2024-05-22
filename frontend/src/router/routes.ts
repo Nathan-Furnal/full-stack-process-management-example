d150913153ca3caedb3b91905b872db4c8a6f22f
Nathan Furnal <nathanfurnal.dev@gmail.com>
2024-05-23 00:40:04 +0200

    update routes wih record view

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -1,11 +1,13 @@
 import HomeView from '@/views/HomeView.vue'
 import VisualizeView from '@/views/VisualizeView.vue'
+import RecordView from '@/views/RecordView.vue'
 
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   { path: '/', component: HomeView },
-  { path: '/processes/visualize', component: VisualizeView }
+  { path: '/processes/visualize', component: VisualizeView },
+  { path: '/processes/record', component: RecordView }
 ]
 
 /* For router: see https://router.vuejs.org/guide/ */
